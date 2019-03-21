15d9f8ac7df2bcb7cd23d04ef15372a158e69a7e
Ilya Safonov <safonov.ilya@gmail.com>
2019-03-21 22:11:21 +0300

    floodProtection

--- bot/index.js
+++ bot/index.js
@@ -3,16 +3,16 @@ console.log(TOKEN);
 const TeleBot = require('telebot');
 const bot = new TeleBot({
     token: TOKEN, // Required. Telegram Bot API token.
-   /* webhook: { // Optional. Use webhook instead of polling.
-        key: 'key.pem', // Optional. Private key for server.
-        cert: 'cert.pem', // Optional. Public key.
-        url: 'https://....', // HTTPS url to send updates to.
-        host: '0.0.0.0', // Webhook server host.
-        port: 443, // Server port.
-        maxConnections: 40 // Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
-    },*/
+    /* webhook: { // Optional. Use webhook instead of polling.
+         key: 'key.pem', // Optional. Private key for server.
+         cert: 'cert.pem', // Optional. Public key.
+         url: 'https://....', // HTTPS url to send updates to.
+         host: '0.0.0.0', // Webhook server host.
+         port: 443, // Server port.
+         maxConnections: 40 // Optional. Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+     },*/
     allowedUpdates: [], // Optional. List the types of updates you want your bot to receive. Specify an empty list to receive all updates.
-    usePlugins: [ 'bigben', 'angrybot'], // Optional. Use user plugins from pluginFolder.
+    usePlugins: ['floodProtection', 'bigben', 'angrybot'], // Optional. Use user plugins from pluginFolder.
     pluginFolder: `${__dirname}/plugins/`, // Optional. Plugin folder location.
     pluginConfig: { // Optional. Plugin configuration.
         // myPluginName: {
---
+++ bot/plugins/floodProtection.js
@@ -0,0 +1,60 @@
+/*
+    Simple flood protection plugin.
+    Note: Received Telegram message time accuracy is one second.
+*/
+
+const userList = {};
+
+// Export bot module
+module.exports = {
+
+    id: 'floodProtection',
+    defaultConfig: {
+        stickerInterval: 300,
+        message: 'Too many messages, relax!'
+    },
+
+    plugin(bot, pluginConfig) {
+
+        const interval = Number(pluginConfig.stickerInterval) || 1;
+        const text = pluginConfig.message;
+
+        bot.mod('message', (data) => {
+
+            const {message} = data;
+            const msg = message;
+            const id = msg.from.id;
+            const user = userList[id];
+            const now = new Date(msg.date);
+
+            if (user && message.sticker) {
+
+                const diff = now - user.lastTime;
+                console.log(diff);
+                user.lastTime = now;
+
+                if (diff <=  interval) {
+
+                    if (text) bot.sendMessage(id, text);
+                    user.flood = true;
+                    const {chat} = message;
+                    if (chat && message.sticker) {
+                        bot.deleteMessage(chat.id, data.message.message_id);
+                        bot.sendMessage(chat.id, 'flood sticker protection, 1 в 5 минут');
+                    }
+
+                    userList[id] = {lastTime: now};
+                    data.message = {};
+
+                }
+
+            } else {
+                userList[id] = {lastTime: now};
+            }
+
+            return data;
+
+        });
+
+    }
+};
